d73e713756b5d58dbfac9b4ff616cd261a43d44a
Tri Vuong <trivektor@gmail.com>
2020-05-31 02:45:27 -0700

    Add map view to Launches

--- components/launches/launches.js
+++ components/launches/launches.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
+// https://medium.com/@clarkjohnson_85334/creating-a-simple-react-native-maps-app-7e3fc324a0b7
+import MapView from 'react-native-maps';
 import {Container, Text} from 'native-base';
 
 const Launches = ({launches}) => {
   return (
     <Container>
-      <Text>Launches: {launches.length}</Text>
+      <MapView style={{...StyleSheet.absoluteFillObject}}>
+        </MapView>
     </Container>
   );
 };
